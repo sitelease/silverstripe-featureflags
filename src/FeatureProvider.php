aa74f487699c0409066fb99f390818a548003ca4
Benjamin Blake <ben@sitelease.ca>
2023-07-10 13:47:34 -0600

    FeatureProvider - Added doc block

--- src/FeatureProvider.php
+++ src/FeatureProvider.php
@@ -12,6 +12,14 @@ use SilverStripe\View\TemplateGlobalProvider;
  */
 class FeatureProvider implements TemplateGlobalProvider
 {
+    /**
+     * Check to see if a feature flag is enabled in the
+     * current silverstripe instance
+     *
+     * @param string $code The feature code to check
+     * @param null|array $context An context object or array that the feature will check
+     * @return boolean
+     */
     public static function isFeatureEnabled(string $code, $context = null): bool
     {
         return Injector::inst()->get(FeatureFlagChecker::class)->isEnabled($code, $context);
