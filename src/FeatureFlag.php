3633b0c74e8c96ab7a91415ad96a4741a86f0c49
Benjamin Blake <ben@sitelease.ca>
2023-12-05 13:16:00 -0700

    FeatureFlag - Fixed member is null error
    + Added a conditional check for members that will prevent the dev build task from erroring out

--- src/FeatureFlag.php
+++ src/FeatureFlag.php
@@ -244,12 +244,13 @@ class FeatureFlag extends DataObject implements PermissionProvider
     {
         parent::onAfterWrite();
         $member = Security::getCurrentUser();
-
-        $historyRecord = FeatureFlagHistory::create();
-        $historyRecord->EnableMode = $this->EnableMode;
-        $historyRecord->AuthorID = $member->ID;
-        $historyRecord->FeatureFlagID = $this->ID;
-        $historyRecord->write();
+        if ($member) {
+            $historyRecord = FeatureFlagHistory::create();
+            $historyRecord->EnableMode = $this->EnableMode;
+            $historyRecord->AuthorID = $member->ID;
+            $historyRecord->FeatureFlagID = $this->ID;
+            $historyRecord->write();
+        }
     }
 
     public function providePermissions()
