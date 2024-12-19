fa2eb90e3395098d4d9f4c4e571fab3549876d90
Benjamin Blake <ben@sitelease.ca>
2024-12-19 13:27:56 -0700

    FeatureFlagChecker - Cached all feature check queries to improve performance

--- src/FeatureFlagChecker.php
+++ src/FeatureFlagChecker.php
@@ -8,34 +8,63 @@ namespace Sitelease\FeatureFlags;
  */
 class FeatureFlagChecker implements FeatureFlagCheckable
 {
+    public static $featureRecordCache;
+
+    /**
+     * Returns a cached array of features
+     *
+     * If the cache is empty this will also hydrate it
+     *
+     * @return array
+     */
+    public static function getCachedFeatures(): array
+    {
+        $cache = self::$featureRecordCache;
+        if (empty($cache)) {
+            print("Cache called");
+            $cache = FeatureFlag::get()->map('Code', 'EnableMode')->toArray();
+            self::$featureRecordCache = $cache;
+        }
+        return $cache;
+    }
+
     public static function isEnabled(string $code, $context = null): bool
     {
-        $feature = FeatureFlag::get()->filter([ 'Code' => $code ])->first();
-
-        if ($feature) {
+        $features = self::getCachedFeatures();
+        if (isset($features[$code])) {
+            $feature = $features[$code];
             // Simple modes
-            if ($feature->EnableMode === 'On') {
+            if ($feature === 'On') {
                 return true;
-            }
-            if ($feature->EnableMode === 'Off') {
+            } elseif ($feature === 'Off') {
                 return false;
+            } else {
+                throw new \LogicException(
+                    "Feature mode not supported - The $code feature is set to $feature which is not supported."
+                    .' You must set it to "On" or "Off" in the feature manager'
+                );
             }
 
             // TODO: validate context
-            if (isset($context)) {
-                // Check each context value against the selections
-                foreach ($context as $key => $obj) {
-                    $contextTest = $feature->Items()->filter([
-                        'ContextKey' => $key,
-                        'ContextID' => $obj ? $obj->ID : 0,
-                    ]);
+            // if (isset($context)) {
+            //     // Check each context value against the selections
+            //     foreach ($context as $key => $obj) {
+            //         $contextTest = $feature->Items()->filter([
+            //             'ContextKey' => $key,
+            //             'ContextID' => $obj ? $obj->ID : 0,
+            //         ]);
 
-                    // Any context match will result in the feature being enabled
-                    if ($contextTest->count() > 0) {
-                        return true;
-                    }
-                }
-            }
+            //         // Any context match will result in the feature being enabled
+            //         if ($contextTest->count() > 0) {
+            //             return true;
+            //         }
+            //     }
+            // }
+        } else {
+            throw new \LogicException(
+                "Unknown feature check requested - A feature check was requested for a feature code of: $code."
+                . ' No feature record exists for that code in the system.'
+            );
         }
 
         return false;
